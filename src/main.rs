16938e482236e6f7cf614078a04d37a8148720d7
orhnk <utfeightt@gmail.com>
2024-04-29 21:34:44 +0300

    src: init db

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use embd;
+use serde_json::{Result, Value};
+
+mod table;
+mod utils;
+
 fn main() {
-    println!("Hello, world!");
+    let db_unformatted = embd::string!("../db/PeriodicTableJSON.json");
+    let db_formatted: Value = serde_json::from_str(&db_unformatted).unwrap();
+    let db = &db_formatted["elements"];
 }
