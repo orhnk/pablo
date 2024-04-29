a7a51ad98dab7eb2ba2d1d70256f3961450613be
orhnk <utfeightt@gmail.com>
2024-04-29 21:36:40 +0300

    feat: add basic utils

---
+++ src/utils.rs
@@ -0,0 +1,8 @@
+// Algo:
+// a grid of squares to draw the elements on (standard: 9x18)
+// a recipe representing fine shape of the periodic table (not a 9x18 square)
+// an order to fullfill the table
+
+mod ptable_repr {
+    // const STANDARD: [(isize, isize)] = [];
+}
